5fbe9f786d71e3b1c216e0a5ce803abf759b71ec
Vinit Gaikwad <vinitgaikwad.sit.comp@gmail.com>
2026-02-26 16:02:32 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -263,5 +263,8 @@ def disease_prediction():
 
 
 # ===============================================================================================
-if __name__ == '__main__':
-    app.run(debug=False)
+
+  if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
